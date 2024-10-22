478a5b56882ae91f8191baf9f8beef62d5a618f3
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 13:08:03 +0200

    1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,30 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        
+        System.out.println("client partito");
+        Socket s = new Socket("localhost", 3000);
+        System.out.println("il client è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        //
+
+        Scanner scan = new Scanner(System.in); //crela l' input
+
+        System.out.println("cercare di indovinare il numero");
+        System.out.println("digitare il numero: ");
+        String stringNumero = scan.nextLine(); //tentativo
+        
     }
 }
