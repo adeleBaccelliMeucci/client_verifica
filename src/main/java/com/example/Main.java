c2ba8757df953f1e969bfa20d4188a5ad30c9a70
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:27:12 +0200

    fine

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        
+
         System.out.println("client partito");
         Socket s = new Socket("localhost", 3000);
         System.out.println("il client è collegato");
@@ -20,11 +20,40 @@ public class Main {
 
         //
 
-        Scanner scan = new Scanner(System.in); //crela l' input
+        Scanner scan = new Scanner(System.in); // crela l' input
 
-        System.out.println("cercare di indovinare il numero");
-        System.out.println("digitare il numero: ");
-        String stringNumero = scan.nextLine(); //tentativo
+        System.out.println("cercare di indovinare il numero tra 0 a 99");
+
         
+        do {
+            System.out.println("digitare il numero: ");
+            String stringNumero = scan.nextLine(); // tentativo
+            out.writeBytes(stringNumero + "\n"); // manda il tentativo
+
+            String risultato = in.readLine(); // tra confronto dei numeri //legge
+            //System.out.println("risposta: " + risultato); // ?=errore da controllare prima //poiu COMMENTARE
+            
+            if (risultato.equals("<")){
+                System.out.println("Troppo piccolo");
+
+            }else if (risultato.equals(">")){
+                System.out.println("Troppo grande");
+
+            }else if(risultato.equals("=")){
+                System.out.println("INDOVINATO!");
+                break;
+            }else if(risultato.equals("?")){
+                System.out.println("Il numero inserito non è valido");
+            }else{
+                System.out.println("ERRORE");
+            }
+        } while (true);
+        String tentativi = in.readLine();
+        System.out.println("hai impiegato " + tentativi + " tentativi");
+
+        
+        s.close();
+
+
     }
 }
